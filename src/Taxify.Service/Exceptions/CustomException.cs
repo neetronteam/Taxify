6f162c1e9d67c8cbe9b46fc250f3e94e6328f8bc
abbosZoirov1 <122oszoirov@gmail.com>
2023-09-17 16:54:52 +0500

    Added Exceptions

---
+++ src/Taxify.Service/Exceptions/CustomException.cs
@@ -0,0 +1,11 @@
+namespace Taxify.Service.Exceptions;
+
+public class CustomException:Exception
+{
+    public int StatusCode { get; set; } 
+    
+    public CustomException(string message,int statusCode):base(message)
+    {
+        StatusCode = statusCode;
+    }
+}
