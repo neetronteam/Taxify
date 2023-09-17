c1bac81dcace6b65372f0b84e6c1890753f32eff
abbosZoirov1 <122oszoirov@gmail.com>
2023-09-17 16:45:41 +0500

    Added Helpers

---
+++ src/Taxify.Service/Helpers/CodeGenerator.cs
@@ -0,0 +1,10 @@
+namespace Taxify.Service.Helpers;
+
+public class CodeGenerator
+{
+    public static int GenerateRandomNumber()
+    {
+        Random random = new Random();
+        return random.Next(100000, 999999);
+    }
+}
---
+++ src/Taxify.Service/Helpers/PathHelper.cs
@@ -0,0 +1,6 @@
+namespace Taxify.Service.Helpers;
+
+public static class PathHelper
+{
+    public static string WebRootPath { get; set; }
+}
