9d9ad4486b6128145fab115945ff21c090027629
Iskandar Qodirov <116558882+itsdotnet@users.noreply.github.com>
2023-09-17 15:24:51 +0500

    Update IRepository.cs

--- src/Taxify.DataAccess/Contracts/IRepository.cs
+++ src/Taxify.DataAccess/Contracts/IRepository.cs
@@ -9,7 +9,6 @@ public interface IRepository<TEntity> where TEntity : Auditable
     ValueTask<TEntity> Update(TEntity entity);
     ValueTask<bool> Delete(TEntity entity);
     ValueTask<TEntity> SelectAsync(Expression<Func<TEntity, bool>> expression, string[] includes = null);
-
     IQueryable<TEntity> SelectAll(Expression<Func<TEntity, bool>> expression = null, string[] includes = null,
         bool isTracking = true);
 }
