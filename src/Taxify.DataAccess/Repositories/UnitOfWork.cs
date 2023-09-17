a7d5ae6d55875a922a5b34cffc6621fbd6192675
abbosZoirov1 <122oszoirov@gmail.com>
2023-09-17 15:59:53 +0500

    Added UnitOfWork

---
+++ src/Taxify.DataAccess/Repositories/UnitOfWork.cs
@@ -0,0 +1,41 @@
+using Taxify.DataAccess.Contexts;
+using Taxify.DataAccess.Contracts;
+using Taxify.Domain.Entities;
+
+namespace Taxify.DataAccess.Repositories;
+
+public class UnitOfWork:IUnitOfWork
+{
+    private readonly TaxifyDbContext _context;
+
+    public UnitOfWork(TaxifyDbContext context)
+    {
+        _context = context;
+        CarRepository = new Repository<Car>(_context);
+        CarModelRepository = new Repository<CarModel>(_context);
+        ColorRepository = new Repository<Color>(_context);
+        DriveRepository = new Repository<Drive>(_context);
+        DriverRepository = new Repository<Driver>(_context);
+        MessageRepository = new Repository<Message>(_context);
+        OrderRepository = new Repository<Order>(_context);
+        UserRepository = new Repository<User>(_context);
+        VehicleRepository = new Repository<Vehicle>(_context);
+    }
+
+    public IRepository<Car> CarRepository { get; }
+    public IRepository<CarModel> CarModelRepository { get; }
+    public IRepository<Color> ColorRepository { get; }
+    public IRepository<Drive> DriveRepository { get; }
+    public IRepository<Driver> DriverRepository { get; }
+    public IRepository<Message> MessageRepository { get; }
+    public IRepository<Order> OrderRepository { get; }
+    public IRepository<User> UserRepository { get; }
+    public IRepository<Vehicle> VehicleRepository { get; }
+
+    public async ValueTask SaveAsync() => await _context.SaveChangesAsync();
+    
+    public void Dispose()
+    {
+        GC.SuppressFinalize(true);
+    }
+}
