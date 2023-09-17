42c9529f132a544ab99e82b229d999592aef7c67
abbosZoirov1 <122oszoirov@gmail.com>
2023-09-17 15:54:10 +0500

    Added IUnitOfWork

---
+++ src/Taxify.DataAccess/Contracts/IUnitOfWork.cs
@@ -0,0 +1,18 @@
+using Taxify.Domain.Entities;
+
+namespace Taxify.DataAccess.Contracts;
+
+public interface IUnitOfWork:IDisposable
+{
+    IRepository<Car> CarRepository { get; }
+    IRepository<CarModel> CarModelRepository { get; }
+    IRepository<Color> ColorRepository { get; }
+    IRepository<Drive> DriveRepository { get; }
+    IRepository<Driver> DriverRepository { get; }
+    IRepository<Message> MessageRepository { get; }
+    IRepository<Order> OrderRepository { get; }
+    IRepository<User> UserRepository { get; }
+    IRepository<Vehicle> VehicleRepository { get; }
+
+    ValueTask SaveAsync();
+}
