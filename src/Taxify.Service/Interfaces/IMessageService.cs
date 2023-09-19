b3765c46774f72c1dbef6654097732db9516236d
Nurullo <ghostone234qwer@gmail.com>
2023-09-19 10:50:37 +0500

    Added Message interface

---
+++ src/Taxify.Service/Interfaces/IMessageService.cs
@@ -0,0 +1,13 @@
+﻿using Taxify.Service.DTOs.Messages;
+
+namespace Taxify.Service.Interfaces;
+
+public interface IMessageService
+{
+    ValueTask<MessageResultDto> AddAsync(MessageCreationDto dto);
+    ValueTask<MessageResultDto> ModifyAsync(MessageUpdateDto dto);
+    ValueTask<bool> RemoveAsync(long id);
+    ValueTask<bool> DestroyAsync(long id);
+    ValueTask<MessageResultDto> RetrieveAsync(long id);
+    ValueTask<IEnumerable<MessageResultDto>> RetrieveAllAsync();
+}
